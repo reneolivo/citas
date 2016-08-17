8f047480aba7c1accdb9967e6061c2dbd36a5656
René Olivo <minet.ws@gmail.com>
2016-08-17 17:21:20 -0400

    add router and menu

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,3 +1,15 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+
+    config.map([
+      { route: [ '', 'appointments' ], name: 'appointments', title: 'Citas', moduleId: 'appointments/index', nav: true },
+      { route: 'professionals', name: 'professionals', title: 'Doctores', moduleId: 'professionals/index', nav: true },
+      { route: 'clients', name: 'clients', title: 'Pacientes', moduleId: 'clients/index', nav: true }
+    ]);
+  }
 }
