ec7cc66961e7960b23992b869b8d1124804f4190
René Olivo <minet.ws@gmail.com>
2016-08-24 12:22:42 -0400

    add future appointment count

--- common/models/professional.js
+++ common/models/professional.js
@@ -1,5 +1,32 @@
 var app = require('../../server/server');
 
 module.exports = function(Professional) {
-
+  Professional.observe('loaded', (context, next) => {
+    appendFutureAppointmentsCount(context).then(
+      () => next(),
+      (error) => next(error)
+    );
+  });
 };
+
+function appendFutureAppointmentsCount(context) {
+  var professional = context.data;
+
+  if (!professional) return Promise.resolve();
+
+  return app.models.Availability.find({
+    where: { professionalId: professional.id }
+  })
+  .then((results) => results.map((r) => r.id))
+  .then((ids) => {
+    return app.models.Appointment.find({ // count is not working
+      where: {
+        date: { gt: new Date() },
+        availabilityId: { inq: ids }
+      }
+    });
+  })
+  .then((results) => {
+    professional.futureAppointmentsCount = results.length;
+  });
+}
