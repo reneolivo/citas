748c53f167d9d9b28415695406be157c41a97373
René Olivo <minet.ws@gmail.com>
2016-09-03 09:55:55 -0400

    add deleting records capability for endpoint manager

--- client/src/resources/elements/endpoint-manager.spec.ts
+++ client/src/resources/elements/endpoint-manager.spec.ts
@@ -4,6 +4,7 @@ describe('EndpointManager', () => {
   let element;
   let apiService;
   let records;
+  let toast;
 
   beforeEach(() => {
     records = [
@@ -20,8 +21,15 @@ describe('EndpointManager', () => {
     ]);
 
     apiService.getAll.and.returnValue(Promise.resolve(records));
+    apiService.delete.and.returnValue(Promise.resolve());
 
-    element = new EndpointManager();
+    toast = jasmine.createSpyObj('Toast', [
+      'success',
+      'error',
+      'warning'
+    ]);
+
+    element = new EndpointManager(toast);
     element.apiService = apiService;
   });
 
@@ -53,6 +61,87 @@ describe('EndpointManager', () => {
       element.apiServiceChanged(apiService);
       setTimeout(() => {
         expect(element.records).toBe(records);
+        next();
+      }, 0);
+    });
+  });
+
+  describe('deleting records', () => {
+    beforeEach(() => {
+      element.selectedRecords = [
+        records[ 0 ],
+        records[ 2 ]
+      ];
+    });
+
+    it('should delete all selected records', () => {
+      element.deleteSelectedRecords();
+
+      expect(apiService.delete).toHaveBeenCalledWith(records[0].id);
+      expect(apiService.delete).toHaveBeenCalledWith(records[2].id);
+    });
+
+    it('should not delete non-selected records', () => {
+      element.deleteSelectedRecords();
+
+      expect(apiService.delete).not.toHaveBeenCalledWith(records[1].id);
+    });
+
+    it(`should display ONE success message when
+      the records have been deleted`, (next) => {
+      element.deleteSelectedRecords();
+
+      setTimeout(() => {
+        expect(toast.success).toHaveBeenCalled();
+        expect(toast.success.calls.count()).toBe(1);
+        next();
+      }, 0);
+    });
+
+    it(`should display ONE error message when
+      any of the records have not been deleted successfully`, (next) => {
+      apiService.delete.and.returnValue(
+        Promise.reject(new Error('not-ok')
+      ));
+      element.deleteSelectedRecords();
+
+      setTimeout(() => {
+        expect(toast.error).toHaveBeenCalled();
+        expect(toast.error.calls.count()).toBe(1);
+        next();
+      }, 0);
+    });
+
+    it(`should clear the selected records after
+      deleting them successfully`, (next) => {
+      element.deleteSelectedRecords();
+
+      setTimeout(() => {
+        expect(element.selectedRecords).toEqual([]);
+        next();
+      }, 0);
+    });
+
+    it(`should not clear the selected records
+      if they could not be deleted successfully`, (next) => {
+      apiService.delete.and.returnValue(
+        Promise.reject(new Error('not-ok')
+      ));
+      element.deleteSelectedRecords();
+
+      setTimeout(() => {
+        expect(element.selectedRecords).not.toEqual([]);
+        next();
+      }, 0);
+    });
+
+    it(`should reload the list of records after
+      deleting any record successfully`, (next) => {
+      spyOn(element, 'getAllRecords');
+      element.deleteSelectedRecords();
+
+      setTimeout(() => {
+        expect(element.getAllRecords).toHaveBeenCalled();
         next();
       }, 0);
     });
--- client/src/resources/elements/endpoint-manager.ts
+++ client/src/resources/elements/endpoint-manager.ts
@@ -1,24 +1,57 @@
-import {bindable} from 'aurelia-framework';
+import {autoinject, bindable} from 'aurelia-framework';
 import {CoreApiService} from '../../services/api/core-api-service';
+import Toast from '../../services/helpers/toast';
 
-console.log('FOR REAL?')
-
+@autoinject
 export class EndpointManager {
   records: any[] = [];
+  selectedRecords: any[] = [];
+
   @bindable title: string = null;
-  @bindable addLabel: string = 'Add Record';
+  @bindable addRecordLabel: string = 'Add Record';
+  @bindable deleteRecordsLabel: string = 'Delete Records';
+  @bindable deleteSuccessMessage: string = 'Record deleted successfully';
+  @bindable deleteErrorMessage: string = 'There was an error deleting the record';
+
   @bindable columns: Column = <Column>{};
   @bindable apiService: CoreApiService;
   @bindable formControl: any; // Todo: change to FormControl;
 
+  constructor(protected toast: Toast) {}
 
   apiServiceChanged() {
     this.getAllRecords();
   }
 
-  protected getAllRecords() {
+  getAllRecords() {
     this.apiService.getAll()
     .then((records) => this.records = records);
+  }
+
+  deleteSelectedRecords() {
+    let promises = this.selectedRecords.map((record) => {
+      return this.apiService.delete(record.id);
+    });
+
+    Promise.all(promises)
+    .then(() => this.displayDeleteSuccessMessage())
+    .then(() => this.selectedRecords = [])
+    .then(null, () => this.displayDeleteErrorMessage())
+    .then(() => this.getAllRecords());
+  }
+
+  displayDeleteSuccessMessage() {
+    this.toast.success(`
+      <i class="fa fa-check"></i>
+      ${this.deleteSuccessMessage}
+    `);
+  }
+
+  displayDeleteErrorMessage() {
+    this.toast.error(`
+      <i class="fa fa-check"></i>
+      ${this.deleteErrorMessage}
+    `);
   }
 }
 
